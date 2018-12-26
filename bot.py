90ca0b5275287852733d85584001baf271874dec
MissNela <44345612+MissNela@users.noreply.github.com>
2018-12-26 17:24:29 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,19 @@
 import discord
-from discord.ext.commands import Bot
 from discord.ext import commands
 from discord.ext.commands.cooldowns import BucketType
 import asyncio
-import platform
 import colorsys
 import random
-import os
-import time
-from discord.voice_client import VoiceClient
+import platform
 from discord import Game, Embed, Color, Status, ChannelType
-
+import os
+import functools
+import time
+import datetime
+import requests
+import json
+import praw
+import aiohttp
 
 
 
